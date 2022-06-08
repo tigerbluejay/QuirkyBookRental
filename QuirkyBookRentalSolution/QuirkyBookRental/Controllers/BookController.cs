b208a78396280d3a6e0b992d8838d775d0e47f5a
tigerbluejay <josemariairiarte@gmail.com>
2022-06-08 13:13:13 -0300

    Initial Commit

---
+++ QuirkyBookRentalSolution/QuirkyBookRental/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuirkyBookRental.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult ReleaseMonth(int year, int month)
+        //{
+        //    return Content("Release Date: " + month + "/" + year);
+        //}
+
+        //public ActionResult ReleaseYearAndAuthor(int year, string author)
+        //{
+        //    return Content(author + " " + year);
+        //}
+        
+    }
+    
+}
