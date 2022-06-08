c39b993492530f45018927a419ebe972ba3ac1a2
tigerbluejay <josemariairiarte@gmail.com>
2022-06-08 15:22:10 -0300

    Book and Genre Models and Views

--- QuirkyBookRentalSolution/QuirkyBookRental/App_Start/BundleConfig.cs
+++ QuirkyBookRentalSolution/QuirkyBookRental/App_Start/BundleConfig.cs
@@ -19,8 +19,10 @@ namespace QuirkyBookRental
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bookRentalJs").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/AdminMenu.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
