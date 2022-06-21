8fb3c0e6135793f68bbf66686db3de53c44cbc92
tigerbluejay <josemariairiarte@gmail.com>
2022-06-21 15:52:25 -0300

    Update Membership Type Views and Add Fields to User Database

---
+++ QuirkyBookRentalSolution/QuirkyBookRental/Migrations/202206162114404_MembershipTypeMappingtoDB.cs
@@ -0,0 +1,24 @@
+﻿namespace QuirkyBookRental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MembershipTypeMappingtoDB : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "ChargeRateOneMonth", c => c.Byte(nullable: false));
+            AddColumn("dbo.MembershipTypes", "ChargeRateSixMonth", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "ChargeRateOneMonth1");
+            DropColumn("dbo.MembershipTypes", "ChargeRateSixMonths");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.MembershipTypes", "ChargeRateSixMonths", c => c.Byte(nullable: false));
+            AddColumn("dbo.MembershipTypes", "ChargeRateOneMonth1", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "ChargeRateSixMonth");
+            DropColumn("dbo.MembershipTypes", "ChargeRateOneMonth");
+        }
+    }
+}
--- QuirkyBookRentalSolution/QuirkyBookRental/Models/IdentityModels.cs
+++ QuirkyBookRentalSolution/QuirkyBookRental/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,12 @@ namespace QuirkyBookRental.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Disable { get; set; }
+        public int MembershipTypeId { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -22,6 +29,7 @@ namespace QuirkyBookRental.Models
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
